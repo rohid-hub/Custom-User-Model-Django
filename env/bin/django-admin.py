900c8f84ecd307aed201691c008ec14839e2a388
rohid-hub <rohidwork303@gmail.com>
2020-04-28 23:10:38 +0600

    Custom User Accounts

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/e/projects/web-projects/django/custom-user-account/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
